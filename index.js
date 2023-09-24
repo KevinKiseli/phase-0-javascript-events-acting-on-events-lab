e2977ae8c06288e44d90d80e87a4ffc2b45b5df9
KevinKiseli <kevin.kiseli@student.moringaschool.com>
2023-09-24 12:39:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-// Your code here
+// // Your code here
 const dodger = document.getElementById("dodger");
 
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+    // Get the current left position of the dodger
+    const leftPosition = dodger.style.left.replace("px", "");
+    const left = parseInt(leftPosition, 10);
   
+    // Move the dodger to the left if it's not already at the left edge
     if (left > 0) {
       dodger.style.left = `${left - 1}px`;
     }
-  }
+}
+
 function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+    // Get the current left position of the dodger
+    const leftPosition = dodger.style.left.replace("px", "");
+    const left = parseInt(leftPosition, 10);
     
-  
+    // Move the dodger to the right if it's not already at the left edge
     if (left > 0) {
       dodger.style.left = `${left + 1}px`;
     }
-  }
+}
 
-  document.addEventListener("keydown", function (e) {
+// Listen for key presses to move the dodger left or right
+document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
         moveDodgerLeft();
     }
     else if (e.key === "ArrowRight") {
         moveDodgerRight(); 
     }
-  });
+});
